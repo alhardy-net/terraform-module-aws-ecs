ca3f85fd5fba5595dfd3656964c7645ae387f550
Allan Hardy <al_hardy@live.com.au>
2021-08-27 23:45:40 +1000

    using latest loki

--- variables.tf
+++ variables.tf
@@ -121,5 +121,5 @@ variable "xray_image" {
 variable "fluent_bit_loki_image" {
   description = "The image used for the loki sidecar"
   type        = string
-  default     = "grafana/fluent-bit-plugin-loki:main"
+  default     = "grafana/fluent-bit-plugin-loki:latest"
 }
